42c74a193512b5c318c59475d7ba36cfc7178438
Hotram89 <marion.dournel@gmail.com>
2022-12-19 00:14:22 +0100

    features: home page corrections and signin page start

--- src/pages/SignIn/SignIn.js
+++ src/pages/SignIn/SignIn.js
@@ -1,7 +1,17 @@
 import React from "react";
+import "./Signin.css";
+import userIcon from "../../img/circle-user-solid.svg";
 
 const SignIn = () => {
-  return <div> c'est ici qu'on s'authentifie</div>;
+  return (
+    <main classname="main bg-dark">
+      <section classbame="sign-in-content">
+        <img classname="fa sign-in-icon" src={userIcon} alt="sign in icon" />
+        <h1>Sign In</h1>
+        <form></form>
+      </section>
+    </main>
+  );
 };
 
 export default SignIn;
